fab77b3f2680f82e037c370c4a7f0b824cb6baa8
Foggydk <159024808+Foggydk@users.noreply.github.com>
2024-05-06 14:51:08 +0300

    Update 3.py

--- lab8/3.py
+++ lab8/3.py
@@ -1,34 +1,14 @@
 from PIL import Image, ImageDraw, ImageFont
-otkritki = {
-    "День рождения": "dr.jpg",
-    "Новый год": "ng.jpg",
-    "8 Марта": "8 mar.jpg",
-    "День победы": "denpob.jpg"
-}
 
-prazdnik = input("К какому празднику вам нужна открытка? ")
-name = input("Введите имя того, кторое нужно вписать в открытку: ")
-
-if prazdnik in otkritki:
-    otkritka = otkritki[prazdnik]
-
-    image = Image.open(otkritka)
-
-    draw = ImageDraw.Draw(image)
-
-    font = ImageFont.truetype("ofont.ru_Bubblez-Graffiti.ttf", 20)
-
-    color = (0, 0, 225)
-
-    text_width, text_height = draw.textlength(f"{name}, поздравляю", font=font)
-
-    x = (image.width - text_width) // 2
-    y = (image.height - text_height) // 2
-
-    draw.text((x, y), f"{name}, поздравляю", font=font, fill=color)
-
-    image.save(f"new_{otkritka}", "png")
-
-    print(f"Открытка для {name} сохранена в файл new_{otkritka}.")
-else:
-    print("Извините, у нас нет открытки к указанному празднику.")
+image = Image.open(r'8 mar.jpg')
+    draw = ImageDraw.Draw(party)
+    x = input("введите имя которое нужно вписать: ")
+    text = str(x) + ', поздравляю! '
+    font = r"ofont.ru_Bubblez-Graffiti.ttf"
+    font = ImageFont.truetype(font_path, size=20)
+    y = draw.textlength(text, font=font)
+    z = image.size
+    w = (z[0] // 2) - (y // 2)
+    image.text((w, 10), text, font=font, fill=("#EEE8AA"))
+    image.save(r'C:\Users\Danil\Pycharm\lab8')
+    image.show()
