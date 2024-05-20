22165311fd9172d0e37661dc0e8296d31b477d89
Foggydk <159024808+Foggydk@users.noreply.github.com>
2024-05-20 15:04:15 +0300

    11

--- 1.py
+++ 1.py
@@ -1,13 +1,19 @@
-import json
-with open('products.json') as f:
-    data = json.load(f)
-products = data['products']
-for product in products:
-    print("Название:", product['name'])
-    print("Цена:", product['price'])
-    print("Вес:", product['weight'])
-    if product['available']:
-        print("В наличии")
-    else:
-        print("Нет в наличии!")
-    print()
+class Restaurant:
+    def __init__(self, restaurant_name, cuisine_type):
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+
+    def describe_restaurant(self):
+        print(f"Ресторан {self.restaurant_name} предлагает кухню типа {self.cuisine_type}.")
+
+    def open_restaurant(self):
+        print("Ресторан открыт!")
+
+newRestaurant = Restaurant("Ratatouille", "французская")
+
+print(f"Название ресторана: {newRestaurant.restaurant_name}")
+print(f"Тип кухни: {newRestaurant.cuisine_type}")
+
+
+newRestaurant.describe_restaurant()
+newRestaurant.open_restaurant()
