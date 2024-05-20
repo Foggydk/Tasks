2f55626a604be8fa785cb94280eba2bd9789d553
Foggydk <159024808+Foggydk@users.noreply.github.com>
2024-05-20 15:04:38 +0300

    11

--- 3.py
+++ 3.py
@@ -1,16 +1,19 @@
-with open('en-ru.txt', 'r', encoding='utf-8') as file:
-    lines = file.readlines()
-rus_eng = {}
-for line in lines:
-    if '-' in line:
-        eng_word, ru_tr = line.strip().split('-')
-        for ru_word in ru_tr.split(','):
-            if ru_word.strip() not in rus_eng:
-                rus_eng[ru_word.strip()] = [eng_word]
-            else:
-                if eng_word not in rus_eng[ru_word.strip()]:
-                    rus_eng[ru_word.strip()].append(eng_word)
-with open('ru-en.txt', 'w', encoding='utf-8') as file:
-    for key in sorted(rus_eng.keys()):
-        trans = ','.join(sorted(rus_eng[key]))
-        file.write(f'{key}-{trans}\n')
+class Restaurant:
+    def __init__(self, restaurant_name, cuisine_type, initial_rating):
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+        self.rating = initial_rating
+
+    def describe_restaurant(self):
+        print(f"Ресторан {self.restaurant_name} предлагает кухню типа {self.cuisine_type}. Рейтинг: {self.rating}")
+
+    def update_rating(self, new_rating):
+        self.rating = new_rating
+        print(f"Рейтинг ресторана {self.restaurant_name} обновлен: {self.rating}")
+
+restaurant = Restaurant("La Bella", "итальянская", 4.5)
+restaurant.describe_restaurant()
+
+new_rating = 4.8
+restaurant.update_rating(new_rating)
+restaurant.describe_restaurant()
